76ae63b4e8e47defcd89b7ae87fe6850251a68c2
hande <hande.dann@gmail.com>
2025-01-27 16:11:29 +0300

    data.json v1 & components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Options from "./components/Options";
+import Skills from "./components/Skills";
+import "./index.css";
+import Profile from "./components/Profile";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Options />
+      <Header />
+      <Main />
+      <Skills />
+      <Profile />
+      <Projects />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Options.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Options() {
+  return (
+    <div>
+      <p>DARK MODE</p>
+      <p> | </p>
+      <p>
+        <span>TÜRKÇE</span>'YE GEÇ
+      </p>
+      Options
+    </div>
+  );
+}
+
+export default Options;
