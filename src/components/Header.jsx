3597067ec60cd49e76c18c4462945ac0a57f458b
hande <hande.dann@gmail.com>
2025-01-28 16:59:24 +0300

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,19 @@ function Header() {
   // dile bağlı olarak getText'i her componentte shortcut'la çalıştırıyoruz:
   return (
     <div>
-      <header>
+      <header className="flex justify-between items-end align">
         <div>
-          <h1>{}</h1>
+          <h1 className="flex justify-center items-center w-10 h-10 bg-indigo-200 text-indigo-800 font-bold rounded-full">
+            H
+          </h1>
         </div>
         <nav>
-          <ul>
-            <li>{getText("skills")}</li>
-            <li>{getText("projects")}</li>
-            <li>{getText("profile")}</li>
+          <ul className="flex justify-end items-center gap-12 mt-5">
+            <button>{getText("skills")}</button>
+            <button>{getText("projects")}</button>
+            <button className="border border-indigo-800 px-4 py-2 rounded-md text-indigo-800 dark:text-white font-bold dark:border-white">
+              {getText("hireMe")}
+            </button>
           </ul>
         </nav>
       </header>
