4ba4fb6f1c1074e4551dfbbde4c95795d72bd47e
hande <hande.dann@gmail.com>
2025-01-29 18:52:05 +0300

    Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LanguageContext } from "../context/LanguageContext";
 
 function Profile() {
-  return <div>Profile</div>;
+  const { getText } = useContext(LanguageContext);
+
+  return (
+    <div>
+      <p className="text-2xl font-bold ">{getText("profile")}</p>
+      <div className="py-3 flex gap-32">
+        <div>
+          <p className="text-indigo-800 dark:text-indigo-500 text-lg pb-3">
+            {getText("profileTittle_1")}
+          </p>
+          <div className="flex gap-10">
+            <div className="flex flex-col gap-1 font-bold">
+              <p>{getText("birthDate")}</p>
+              <p>{getText("birthPlace")}</p>
+              <p>{getText("education")}</p>
+              <p>{getText("role")}</p>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p>{getText("birthDate_answer")}</p>
+              <p>{getText("birthPlace_answer")}</p>
+              <p>{getText("education_answer")}</p>
+              <p>{getText("role_answer")}</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/2 flex flex-col justify-start">
+          <p className="text-indigo-800 dark:text-indigo-500 text-lg pb-3">
+            {getText("profileTittle_2")}
+          </p>
+          <p>{getText("aboutMe_1")}</p>
+          <br />
+          <p>{getText("aboutMe_2")}</p>
+        </div>
+      </div>
+      <br />
+      <hr />
+      <br />
+    </div>
+  );
 }
 
 export default Profile;
